c89e67bd73c7fa263bdc3e53e750a9ff0b1369bc
Alexander Murashkin <a.murashkin@carprice.ru>
2018-03-12 12:59:27 +0300

    Работа супервизора

--- app.py
+++ app.py
@@ -1,47 +1,99 @@
-from ftplib import FTP
-import os
+class GlobalStore:
+    def __init__(self, store_host: str, store_port: int, store_type: str = 'FTP',
+                 user: str = '', password: str = ''):
+        self.user = user
+        self.password = password
+
+        # Подключение к глобальному файловому хранилищу
+        self.store_host = store_host
+        self.store_port = store_port
+        self.store_type = store_type
+
+    def load_files(self, files: list) -> list:
+        return []
 
 
-# загрузка файлов в глобальное хранилище
-def load_files(paths: list, host: str, port: int, username: str = '', password: str = '', acct: str = '',
-               output: str = '/tmp/'):
-    ftp = FTP()
-    try:
-        ftp.connect(host, port)
-        ftp.login(username, password, acct)
+class Supervisor:
+    def __init__(self, db_host: str = '', db_port: int = '', store_host: str = '', store_port: int = '',
+                 store_type: str = 'FTP', user: str = '', password: str = ''):
+        self.user = user
+        self.password = password
 
-        for dirName in output.split(os.sep):
-            try:
-                ftp.mkd(dirName)
-            except:
-                pass
-            ftp.cwd(dirName)
+        # Подключение к глобальному файловому хранилищу
+        self.store_host = store_host
+        self.store_port = store_port
+        self.store_type = store_type
 
-        for filename in paths:
-            try:
-                f = open(filename, "rb")
-                ftp.storbinary("STOR " + filename, f)
-            finally:
-                try:
-                    f.close()
-                except:
-                    pass
-    finally:
-        try:
-            ftp.quit()
-        except:
-            pass
+        # Подключение к базе (серверу), где хранится вся информация о разметке, мета информация и т.п.
+        self.db_host = db_host
+        self.db_port = db_port
+
+    '''
+    Отправка файлов в глобальное хранилище
+    '''
+
+    def send_files(self, files: list) -> list:
+        gs = GlobalStore(self.store_host, self.store_port, self.store_type, self.user, self.password)
+        hashes = gs.load_files(files)
+        return hashes
+
+    '''
+    Получение списка файлов из глобального хранилища со всей необходимой информацией
+    id файла
+    id оператора, который будет или уже разметил файл
+    статус (в работе, размечен, подтвержден супервизором)
+    '''
+
+    def get_store(self):
+        return {}
+
+    '''
+    Получение списка операторов, для которых будет назначаться работа
+    '''
+
+    def get_operators(self):
+        return {}
+
+    '''
+    Для набора изображений назначаем оператора, который будет их размечать
+    '''
+
+    def set_operator(self, operator_id: int, img_hashes: list):
+        pass
+
+    '''
+    Получение размеченного изображения. Самого файла, мета данных и данных разметки.
+    '''
+
+    def get_marked_files(self, img_hashes: list):
+        return {}
+
+    '''
+    Отметка корректной разметки изображения
+    '''
+
+    def set_correct_marked_file(self, img_hash: str):
+        pass
 
 
 def main():
-    host = 'ftp.cse.buffalo.edu'
-    port = 21
-    username = ''
-    password = ''
-    acct = ''
+    visor = Supervisor()
     paths = ['/home/amurashkin/1.txt', '/home/amurashkin/2.txt']
-    output = '/data/ftp/datasets/test/'
-    load_files(paths, host, port, username, password, acct, output)
+    # Набор изображений загрузить в глобальное хранилище.
+    visor.send_files(paths)
+
+    # Выбрать множество изображений.
+    # Выбрать объекты, которые необходимо разметить на данном множестве.
+    files = visor.get_store()
+    ids = ['fa92130', 'ee3321f94d']
+    # Назначить оператора для разметки некоторых объектов.
+    operators = visor.get_operators()
+    operator_id = 123
+    visor.set_operator(operator_id, ids)
+
+    result = visor.get_marked_files(ids)
+
+    visor.set_correct_marked_file(ids[0])
 
 
 if __name__ == "__main__":
